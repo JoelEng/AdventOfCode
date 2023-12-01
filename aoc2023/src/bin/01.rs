e6623905faf8cee77d9f6f605bfa063e70cfb324
JoelEng <joel.n.engstrom@protonmail.com>
2023-12-01 10:47:11 +0100

    2023 day 01 clearer

--- aoc2023/src/bin/01.rs
+++ aoc2023/src/bin/01.rs
@@ -8,21 +8,25 @@ fn main(input: &str) -> (u32, u32) {
 }
 
 fn sum(input: &str, part2: bool) -> u32 {
-    input
-        .lines()
-        .map(|l| {
-            l.chars()
-                .enumerate()
-                .filter_map(|(pos, c)| match c {
-                    d if d.is_digit(10) => d.to_digit(10),
-                    _ if part2 => WORDS
-                        .iter()
-                        .enumerate()
-                        .find_map(|(i, w)| l[pos..].starts_with(w).then_some(i as u32 + 1)),
-                    _ => None,
-                })
-                .collect::<Vec<u32>>()
-        })
-        .filter_map(|l| Some(l.first()? * 10 + l.last()?))
-        .sum()
+    input.lines().filter_map(|l| line_val(l, part2)).sum()
+}
+
+fn line_val(line: &str, part2: bool) -> Option<u32> {
+    let v: Vec<u32> = line
+        .chars()
+        .enumerate()
+        .filter_map(|(pos, c)| val(line, pos, c, part2))
+        .collect();
+    Some(v.first()? * 10 + v.last()?)
+}
+
+fn val(line: &str, pos: usize, c: char, part2: bool) -> Option<u32> {
+    match c {
+        d if d.is_digit(10) => d.to_digit(10),
+        _ if part2 => WORDS
+            .iter()
+            .enumerate()
+            .find_map(|(i, w)| line[pos..].starts_with(w).then_some(i as u32 + 1)),
+        _ => None,
+    }
 }
