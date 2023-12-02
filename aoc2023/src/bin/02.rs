83fe477f1a6d9040895da884ab6d0907cf45848c
JoelEng <joel.n.engstrom@protonmail.com>
2023-12-02 12:46:29 +0100

    2023 day 02

--- aoc2023/src/bin/02.rs
+++ aoc2023/src/bin/02.rs
@@ -2,51 +2,25 @@ use regex::Regex;
 
 #[aors::main]
 fn main(input: &str) -> (usize, u32) {
-    let games: Vec<Vec<&str>> = input
+    let red = Regex::new("([0-9]+) red").ok().unwrap();
+    let green = Regex::new("([0-9]+) green").ok().unwrap();
+    let blue = Regex::new("([0-9]+) blue").ok().unwrap();
+
+    let games = input
         .lines()
-        .filter_map(|l| l.split(":").last())
-        .map(|g| g.split(";").collect())
-        .collect();
+        .map(|g| (count(g, &red), count(g, &green), count(g, &blue)));
     let p1 = games
-        .iter()
+        .clone()
         .enumerate()
-        .filter(|(_, g)| game(g))
+        .filter(|(_, (r, g, b))| *r <= 12 && *g <= 13 && *b <= 14)
         .map(|(i, _)| i + 1)
         .sum();
-    let p2 = games.iter().map(|g| game2(g)).sum();
-    (p1, p2)
+    (p1, games.map(|(r, g, b)| r * g * b).sum())
 }
 
-fn game(g: &Vec<&str>) -> bool {
-    let red = Regex::new("([0-9]+) red").ok().unwrap();
-    let green = Regex::new("([0-9]+) green").ok().unwrap();
-    let blue = Regex::new("([0-9]+) blue").ok().unwrap();
-
-    possible(g, &red, 12) && (possible(g, &green, 13)) && (possible(g, &blue, 14))
-}
-
-fn possible(g: &Vec<&str>, r: &Regex, max: u32) -> bool {
-    let n: Option<u32> = g
-        .iter()
-        .filter_map(|s| r.captures(s)?.get(1)?.as_str().parse().ok())
-        .max();
-    if let Some(n) = n {
-        return n <= max;
-    }
-    false
-}
-
-fn game2(g: &Vec<&str>) -> u32 {
-    let red = Regex::new("([0-9]+) red").ok().unwrap();
-    let green = Regex::new("([0-9]+) green").ok().unwrap();
-    let blue = Regex::new("([0-9]+) blue").ok().unwrap();
-
-    possible2(g, &red) * (possible2(g, &green)) * (possible2(g, &blue))
-}
-
-fn possible2(g: &Vec<&str>, r: &Regex) -> u32 {
-    g.iter()
-        .filter_map(|s| r.captures(s)?.get(1)?.as_str().parse().ok())
+fn count(g: &str, re: &Regex) -> u32 {
+    re.captures_iter(g)
+        .filter_map(|c| c.get(1)?.as_str().parse().ok())
         .max()
         .unwrap_or(0)
 }
