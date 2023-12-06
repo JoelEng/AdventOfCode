334e0753b4c2bab681f9d6ec1bd0e109e5cc8cb9
JoelEng <joel.n.engstrom@protonmail.com>
2023-12-06 17:55:03 +0100

    2023 day 06

--- aoc2023/src/bin/06.rs
+++ aoc2023/src/bin/06.rs
@@ -1,30 +1,22 @@
 use itertools::Itertools;
 
 #[aors::main]
-fn main(input: &str) -> (u128, u128) {
-    let (times, records): (Vec<u128>, Vec<u128>) = input
-        .lines()
-        .map(|l| {
-            l.split_whitespace()
-                .filter_map(|n| n.parse().ok())
-                .collect()
-        })
-        .next_tuple()
-        .unwrap();
-    println!("{:?}", records);
-    let p1 = times.into_iter().zip(records).collect();
-    (run(p1), run(vec![(49787980, 298118510661181)]))
+fn main(input: &str) -> (u32, u32) {
+    let mut a: Vec<f64> = input
+        .split_whitespace()
+        .filter_map(|w| w.parse().ok())
+        .collect();
+    let p1 = a.split_off(a.len() / 2).into_iter().zip(a);
+    let (t, r) = input.lines().map(|l| p2(l)).next_tuple().unwrap();
+    (p1.map(|(r, t)| calc(t, r)).product(), calc(t, r))
 }
 
-fn run(races: Vec<(u128, u128)>) -> u128 {
-    let mut wins = vec![];
-    for (time, record) in races {
-        for hold in 1..time {
-            if (time - hold) * hold > record {
-                wins.push(time - (hold * 2) + 1);
-                break;
-            }
-        }
-    }
-    wins.iter().product()
+fn calc(t: f64, r: f64) -> u32 {
+    t as u32 - 1 - 2 * ((t / 2.0) - ((t / 2.0).powf(2.0) - r).sqrt()) as u32
+}
+
+fn p2(l: &str) -> f64 {
+    l.chars()
+        .filter_map(|c| c.to_digit(10))
+        .fold(0.0, |acc, n| acc * 10.0 + n as f64)
 }
