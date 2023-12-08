f49964852c9d905148b38762c6e0b635bf27d19c
JoelEng <joel.n.engstrom@protonmail.com>
2023-12-08 16:49:12 +0100

    2023 day 08

--- aoc2023/src/bin/08.rs
+++ aoc2023/src/bin/08.rs
@@ -4,75 +4,30 @@ use regex::Regex;
 
 #[aors::main]
 fn main(input: &str) -> (usize, usize) {
-    let dirs: Vec<char> = input.lines().next().unwrap().chars().collect();
+    let d: Vec<char> = input.lines().next().unwrap().chars().collect();
     let re = Regex::new(r"([A-Z0-9]+) = \(([A-Z0-9]+), ([A-Z0-9]+)\)").unwrap();
-    let map: HashMap<String, (String, String)> = re
+    let m: HashMap<&str, (&str, &str)> = re
         .captures_iter(input)
-        .filter_map(|m| {
-            Some((
-                m.get(1)?.as_str().to_string(),
-                (
-                    m.get(2)?.as_str().to_string(),
-                    m.get(3)?.as_str().to_string(),
-                ),
-            ))
-        })
+        .filter_map(|m| Some((m.get(1)?.as_str(), (m.get(2)?.as_str(), m.get(3)?.as_str()))))
         .collect();
 
-    let p1 = run(
-        vec!["AAA".to_string()],
-        vec!["ZZZ".to_string()],
-        &map,
-        &dirs,
-    );
-    let p2 = run(
-        map.clone()
-            .into_keys()
-            .filter(|k| k.ends_with("A"))
-            .collect(),
-        map.clone()
-            .into_keys()
-            .filter(|k| k.ends_with("Z"))
-            .collect(),
-        &map,
-        &dirs,
-    );
-    (p1, p2)
+    let s = m.keys().filter(|k| k.ends_with("A")).map(|k| *k).collect();
+    let e = m.keys().filter(|k| k.ends_with("Z")).map(|k| *k).collect();
+    (run(vec!["AAA"], vec!["ZZZ"], &m, &d), run(s, e, &m, &d))
 }
 
-fn run(
-    start: Vec<String>,
-    end: Vec<String>,
-    map: &HashMap<String, (String, String)>,
-    dirs: &Vec<char>,
-) -> usize {
-    let mut a = 1;
-    for c in start {
-        let cycle_size = cycle(c, &end, &map, &dirs);
-        a = lcm(a, cycle_size);
-    }
-    a
+fn run(s: Vec<&str>, e: Vec<&str>, m: &HashMap<&str, (&str, &str)>, d: &Vec<char>) -> usize {
+    s.iter().fold(1, |acc, s| lcm(acc, cycle(s, &e, &m, &d)))
 }
 
-fn cycle(
-    mut current: String,
-    end: &Vec<String>,
-    map: &HashMap<String, (String, String)>,
-    dirs: &Vec<char>,
-) -> usize {
-    let mut visited = vec![(0, current.clone())];
-    let mut i = 0;
-    loop {
-        // println!("{}", current);
-        current = match dirs[i % dirs.len()] {
-            'L' => map.get(&current).unwrap().0.clone(),
-            _ => map.get(&current).unwrap().1.clone(),
+fn cycle(s: &str, e: &Vec<&str>, m: &HashMap<&str, (&str, &str)>, d: &Vec<char>) -> usize {
+    let (mut current, mut i) = (s, 0);
+    while !e.contains(&current) {
+        current = match d[i % d.len()] {
+            'L' => m.get(&current).unwrap().0,
+            _ => m.get(&current).unwrap().1,
         };
-        if visited.contains(&(i % dirs.len(), current.clone())) {
-            break;
-        }
-        visited.push((i % dirs.len(), current.clone()));
         i += 1;
     }
-    visited.len() - i % dirs.len() - 1
+    i
 }
